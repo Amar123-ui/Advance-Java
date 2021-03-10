cc2f4d4a9865ad056c5df8dd4c95d86a0cb3d6a7
ParthSharma1234 <79687615+ParthSharma1234@users.noreply.github.com>
2021-03-10 11:23:40 +0530

    Add files via upload

---
+++ gridlayout.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.awt.*;
+
+import javax.swing.*;
+    public class GridBagLayout extends JFrame{
+        private final GridBagLayout GridBagLayoutgrid;
+
+        public static void main(String[] args) {
+            GridBagLayout a = new GridBagLayout();
+        }
+        public GridBagLayout() {
+            GridBagLayoutgrid = new GridBagLayout();
+            GridBagConstraints gbc = new GridBagConstraints();
+            LayoutManager grid = null;
+            setLayout(null);
+            setTitle("GridBag Layout Example");
+            GridBagLayout layout = new GridBagLayout();
+            this.setLayout((LayoutManager) layout);
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            this.add(new Button("Button One"), gbc);
+            gbc.gridx = 1;
+            gbc.gridy = 0;
+            this.add(new Button("Button two"), gbc);
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            gbc.ipady = 20;
+            gbc.gridx = 0;
+            gbc.gridy = 1;
+            this.add(new Button("Button Three"), gbc);
+            gbc.gridx = 1;
+            gbc.gridy = 1;
+            this.add(new Button("Button Four"), gbc);
+            gbc.gridx = 0;
+            gbc.gridy = 2;
+            gbc.fill = GridBagConstraints.HORIZONTAL;
+            gbc.gridwidth = 2;
+            this.add(new Button("Button Five"), gbc);
+            setSize(300, 300);
+            setPreferredSize(getSize());
+            setVisible(true);
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        }
+
+    }
+
